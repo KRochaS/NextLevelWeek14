1d64fdf168b4c6308154a91557ea9f0510b9b2e0
KRochaS <karinerocha.s@hotmail.com>
2024-02-12 12:42:02 -0300

    feat: Implement VotingPubSub for voting on polls

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from "fastify";
 import { randomUUID } from "node:crypto";
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
     app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -36,6 +38,13 @@ export async function voteOnPoll(app: FastifyInstance) {
                     }
                 })
 
+                const votes = await redis.zincrby(pollId, -1, userPreviewsVoteOnPoll.pollOptionId)
+
+                voting.publish(pollId, {
+                    pollOptionId: userPreviewsVoteOnPoll.pollOptionId,
+                    votes: Number(votes),
+                })
+
             } else if (userPreviewsVoteOnPoll) {
                 return reply.status(400).send({ message: 'You already voted on this poll.' })
             }
@@ -60,7 +69,12 @@ export async function voteOnPoll(app: FastifyInstance) {
             }
         })
 
+        const votes = await redis.zincrby(pollId, 1, pollOptionId)
 
+        voting.publish(pollId, {
+            pollOptionId,
+            votes: Number(votes),
+        })
 
 
         return reply.status(201).send()
